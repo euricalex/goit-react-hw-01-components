1bb4b6c5dba2c1cb0665637bd4c9098a38b4d32e
Alexander <122707746+euricalex@users.noreply.github.com>
2023-08-02 01:32:59 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import { QuizList } from "./QuizList";
 import quizItems from '../data.json';
+import { SearchBar } from "../SearchBar";
 
 export const App = () => {
-  return ( <div><QuizList items={quizItems}/></div>
+  return ( <>
+    <SearchBar/>
+    <QuizList items={quizItems}/></>
   );
 };
